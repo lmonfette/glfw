a9b2b747e07ce74d0c21fd815d6394e130a9199b
lmonfette <lmonfette@parilium-technologies.com>
2023-08-21 11:39:31 -0400

    Build and linked succefully

---
+++ premake5.lua
@@ -0,0 +1,50 @@
+project "glfw"
+    kind "StaticLib" -- Static library project
+    language "C"
+    staticruntime "on"
+    
+    -- Add your source files here
+    files {
+        "src/**.h",
+        "src/**.c",
+        "src/**.m"
+    }
+    
+    filter "system:windows"
+        -- Add Windows-specific settings if needed
+
+        defines
+        {
+            "_GLFW_WIN32"
+        }
+    
+    filter "system:linux"
+        -- Add Linux-specific settings if needed
+
+        defines
+        {
+            "_GLFW_X11"
+        }
+
+    filter "system:macosx"
+        -- Add MacOSX-specific settings if needed
+
+        links { "X11.framework", "vulkan" }
+
+        defines
+        {
+            "_GLFW_COCOA"
+        }
+
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
